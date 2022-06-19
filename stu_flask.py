9bba4ad6e4e0b8734ed92b74f20ec31a531d4bed
selva kumar <selvakumar1995sri@gmail.com>
2022-06-19 21:38:36 +0530

    sunday commit

--- stu_flask.py
+++ stu_flask.py
@@ -1,29 +1,57 @@
 import json
-from flask import Flask,render_template
+from operator import index, indexOf
+from flask import Flask,render_template, request
+import requests
+
 
 app=Flask(__name__)
 
-@app.rote('/create')
+@app.route('/create',methods=['post'])
 def create():
     x=open('student.json','r')
-    data=json.load(x)
-    student_list=data['name']
-    student_det={}
-    count=1
-    while count<4:
-            key=input()
-            value=input()
-            student_det[key]=value
-            count+=1
-            student_list.append(student_det)
-            print(type(student_list))
-            data.append(student_list)
-            return data
+    data_obj=json.load(x)
+    data_list=data_obj['students']
+    val = request.get_json()
+    data_list.append(val)
+    with open('studen.json','w') as y:
+        json.dump(data_obj,y)
+    return "created successfully"
+
+@app.route('/update',methods=['put'])
+def update():
+    x=open('student.json','r')
+    data_obj=json.load(x)
+    data_list=data_obj["students"]
+    val = request.get_json()
+    for i in data_list:
+        if i['id']==val["id"]:
+            index_i=data_list.index(i)
+            data_list[index_i]=val
+    
+    with open('student.json','w') as y:
+        json.dump(data_obj,y)
+    return "updated successfully"
+
+@app.route('/delete',methods=['delete'])
+def delete():
+    x=open('student.json','r')
+    data_obj=json.load(x)
+    data_list=data_obj['students']
+    val = request.get_json()
+    for i in data_list:
+        if i['id']==val['id']:
+            index_i=data_list.index(i)
+            del data_list[index_i]
+    
+    with open('student.json','w') as y:
+        json.dump(data_obj,y)
+        return "successfully deleted"
 
 @app.route('/viewStudents',methods=['post','get'])
 def read():
     x=open('student.json','r')
     data=json.load(x)
+    x.close()
     return data
     
     
