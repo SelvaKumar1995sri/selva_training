ce0affb052fe6957df144ead94093d24f955ab95
selva kumar <selvakumar1995sri@gmail.com>
2022-06-19 08:08:45 +0530

    commit create

--- stu_flask.py
+++ stu_flask.py
@@ -1,5 +1,31 @@
 import json
+from flask import Flask,render_template
 
-x=open('emp_crud.json')
-data=json.load(x)
-print(data["students"])
+app=Flask(__name__)
+
+@app.rote('/create')
+def create():
+    x=open('student.json','r')
+    data=json.load(x)
+    student_list=data['name']
+    student_det={}
+    count=1
+    while count<4:
+            key=input()
+            value=input()
+            student_det[key]=value
+            count+=1
+            student_list.append(student_det)
+            print(type(student_list))
+            data.append(student_list)
+            return data
+
+@app.route('/viewStudents',methods=['post','get'])
+def read():
+    x=open('student.json','r')
+    data=json.load(x)
+    return data
+    
+    
+if __name__=='__main__':
+    app.run(debug=True)
